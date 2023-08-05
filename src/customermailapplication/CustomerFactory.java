01e6854a5fb2b31d88704c8e19e3a156562037ae
Pphoom-kit 3 <88782114+PPhoomkmitl@users.noreply.github.com>
2023-08-05 17:39:18 +0700

    Add files via upload

---
+++ src/customermailapplication/CustomerFactory.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package customermailapplication;
+
+/**
+ *
+ * @author Phoom1645
+ */
+public class CustomerFactory {
+    public static Customer CreateCustomer(String type){
+        switch(type) {
+            case "Regular":
+                return new RegularCustomer();
+            case "Mountain":
+                return new MountainCustomer();
+            case "Delinquent":
+                return new DelinquentCustomer();     
+        }
+        return null;
+    }
+}
